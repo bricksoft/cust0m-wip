8eb2c6c1876d2882cb632c57914e888c631a3e84
peanutbother <peanutbother@protonmail.com>
2022-03-26 21:51:15 +0100

    use gulp instead

---
+++ gulpfile.ts
@@ -0,0 +1,32 @@
+import { src, dest, parallel } from "gulp";
+import _sass from "sass";
+import gulpSass from "gulp-sass";
+import cleanCSS from "gulp-clean-css";
+import rename from "gulp-rename";
+import typescript from "gulp-typescript";
+
+const sass = gulpSass(_sass);
+
+// content-style
+const styleTask = () =>
+    src("src/content/index.scss", {})
+        // rename first, else output will be in multiple files
+        .pipe(rename("cust0m.css"))
+        // build sass
+        .pipe(sass.sync().on("error", sass.logError))
+        // minify
+        .pipe(cleanCSS())
+        // output
+        .pipe(dest("./dist"));
+
+// content-script
+const scriptTask = () =>
+    src("src/content/index.ts")
+        // transpile
+        .pipe(typescript())
+        // rename
+        .pipe(rename("cust0m.js"))
+        // output
+        .pipe(dest("./dist"));
+
+export default parallel(styleTask, scriptTask);
